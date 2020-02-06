19643bd963c9a3461d18b24ea6c196222fa01377
Greg Caporaso <gregcaporaso@gmail.com>
2020-02-06 10:51:19 -0700

    WIP

--- code/exmp.py
+++ code/exmp.py
@@ -3,6 +3,18 @@ import numpy as np
 
 import qiime2
 
+import seaborn as sns
+import scipy.stats
+import bisect
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from skbio.stats.distance import MissingIDError
+from statsmodels.stats.multitest import multipletests
+
+import matplotlib
+
 base_dir = ".."
 data_dir = os.path.join(base_dir, 'data', 'exmp1-and-exmp2')
 metadata_dir = os.path.join(base_dir, "sample-metadata")
@@ -72,3 +84,78 @@ def week_to_period(week):
         return '4'
     else:
         return '5'
+
+
+
+
+alphas = [(0.001, '***'), (0.01, '**'), (0.05, '*')]
+
+font = {'family' : 'Arial',
+        'weight' : 'normal',
+        'size'   : 8}
+matplotlib.rc('font', **font)
+
+sns.set_style("whitegrid")
+
+def get_sig_text(p, alphas, null_text=""):
+    if np.isnan(p):
+        return null_text
+    alphas.sort()
+    if p >= alphas[-1][0]:
+        return 'ns'
+    sorted_location = bisect.bisect([e[0] for e in alphas], p)
+    return alphas[sorted_location][1]
+
+def plot_week_data(df, metric, time_column, label_axes=True):
+    df[time_column] = pd.to_numeric(df[time_column], errors='coerce')
+    df[metric] = pd.to_numeric(df[metric], errors='coerce')
+
+    df = df.sort_values(by=time_column)
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax = sns.boxplot(data=df, x=time_column, y=metric, color='white', ax=ax)
+    ax = sns.swarmplot(data=df, x=time_column, y=metric, color='black', ax=ax)
+
+    x0 = np.min(df[time_column]) - 1
+    x1 = np.max(df[time_column]) + 1
+
+    if not label_axes:
+        ax.set_xlabel('')
+        ax.set_ylabel('')
+    return fig
+
+# Change in distance to donor from time zero is different than zero
+# (positive t means more different than donor, negative t means more similar to donor)
+def tabulate_week_to_reference_week_paired_stats(df, metric, reference_time, time_column, test_fn=scipy.stats.mannwhitneyu):
+    results = []
+    df[time_column] = pd.to_numeric(df[time_column], errors='coerce')
+    df = df.sort_values(by=time_column)
+    weeks = df[time_column].unique()
+    for i in weeks:
+        reference_time_metric = df[df[time_column] == reference_time][metric]
+        time_i_metric = df[df[time_column] == i][metric]
+        t, p = test_fn(reference_time_metric, time_i_metric)
+        results.append((i, len(time_i_metric), np.median(time_i_metric), t, p))
+    result = pd.DataFrame(results,
+                          columns=[time_column, 'n', metric, 'test-statistic', 'p-value']
+                         )
+    result = result.set_index(time_column)
+    result['q-value'] = multipletests(result['p-value'])[1]
+    return result
+
+def plot_week_data_with_stats(sample_md, metric, time_column, hue=None, alphas=alphas, reference_time=1,
+                              output_figure_filepath=None, output_table_filepath=None):
+    fig = plot_week_data(sample_md, metric, time_column, label_axes=True)
+    stats = tabulate_week_to_reference_week_paired_stats(sample_md, metric, reference_time, time_column)
+    ymax = fig.axes[0].get_ylim()[1]
+    stats.sort_index()
+    for i, w in enumerate(stats.index):
+        t, q = stats['test-statistic'][w], stats['q-value'][w]
+        sig_text = get_sig_text(q, alphas)
+        fig.axes[0].text(i, 1.02*ymax, sig_text, ha='center', va='center')
+    if output_table_filepath is not None:
+        stats.to_csv(output_table_filepath)
+    if output_figure_filepath is not None:
+        fig.savefig(output_figure_filepath, dpi = (300))
+    else:
+        return fig
